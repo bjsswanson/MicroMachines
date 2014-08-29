c99cc0eb2ae6327b361dd1bf9758a1bb2ba1cae8
swansonb <bjsswanson@gmail.com>
2014-08-29 11:55:33 +0100

    Using Mustache

--- app.js
+++ app.js
@@ -1,59 +1,35 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+var mustache = require("mustache");
+ 
+var app = module.exports = express.createServer();
+ 
+// Configuration
+ 
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'mustache');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.compiler({ src: __dirname + '/public'}));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+ 
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
-
-
-module.exports = app;
+ 
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+ 
+// Routes
+ 
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+ 
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
