7d35681c1d36e91c5ff76044a388c95fe687c6a3
swansonb <bjsswanson@gmail.com>
2014-09-03 12:16:30 +0100

    Adding boilerplate for handlebars and express 4

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
 var express = require('express');
-var mustache = require("mustache");
- 
-var app = module.exports = express.createServer();
- 
-// Configuration
- 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'mustache');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.compiler({ src: __dirname + '/public'}));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
- 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
- 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
- 
-// Routes
- 
+var app = express();
+var expressHbs = require('express-handlebars');
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public')); 
+
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+  res.render('index');
 });
- 
+
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
