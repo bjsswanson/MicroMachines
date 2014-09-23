b86f80147e16ba26cf7ae4cdfe41f6c7cf16a5da
Chloe Kaian Lam <chloe.kaian-lam@digitaslbi.com>
2014-09-23 16:06:41 +0100

    Created CameraChase object to be initiated with GamePlay function

--- public/javascripts/MicroMachines.js
+++ public/javascripts/MicroMachines.js
@@ -358,7 +358,8 @@ MicroMachines.Ramp.prototype = function() {
 }();
 
 // Attempting a starting and finishing line
-MicroMachines.Start = function () {
+
+MicroMachines.Start.prototype = function() {
 	this.running = true;
 	this.car.start();
 
@@ -368,17 +369,9 @@ MicroMachines.Start = function () {
 
 	this.state = MicroMachines.STATE_START;
 	this.showResults();
-};
+}();
 
-MicroMachines.Start.prototype = function() {
-	var expose = {
-		constructor: MicroMachines.Start
-	}
-
-	return expose;
-}
-
-MicroMachines.Stop = function () {
+MicroMachines.Stop.prototype = function() {
 	this.running = false;
 	this.car.stop();
 
@@ -388,12 +381,67 @@ MicroMachines.Stop = function () {
 
 	this.state = MicroMachines.STATE_COMPLETE;
 	this.showResults();
+}();
+
+// Setting up camera chasing mechanics 
+
+MicroMachines.CameraChase = function(opts) {
+	this.dir = new THREE.Vector3(0, 0, 1);
+	this.up = new THREE.Vector3(0, 1, 0);
+	this.target = new THREE.Vector3();
+	this.speedOffset = 0;
+	this.speedOffsetMax = 10;
+	this.speedOffsetStep = 0.05;
+
+	this.modes = {
+		CHASE: 0,
+		ORBIT: 1
+	}
+	this.mode = this.modes.CHASE;
+
+	this.camera = opts.camera;
+	this.targetObject = opts.target;
+	this.cameraCube = opts.cameraCube == undefined ? null : opts.cameraCube;
+
+	this.yoffset = opts.yoffset == undefined ? 8.0 : opts.yoffset;
+	this.zoffset = opts.zoffset == undefined ? 10.0 : opts.zoffset;
+	this.viewOffset = opts.viewOffset == undefined ? 10.0 : opts.viewOffset;
+	this.orbitOffset = 12;
+	this.lerp = opts.lerp == undefined ? 0.5 : opts.lerp;
+	this.time = 0.0;
 }
 
-MicroMachines.Stop.prototype = function() {
-	var expose = {
-		constructor: MicroMachines.Stop
+MicroMachines.CameraChase.prototype.update = function(dt, ratio) {
+	if( this.mode == this.modes.CHASE ) {
+		this.dir.set(0, 0, 1);
+		this.up.set(0, 1, 0);
+
+		this.targetObject.matrix.rotateOnAxis(this.up);
+		this.targetObject.matrix.rotateOnAxis(this.dir);
+
+		this.speedOffset += (this.speedOffsetMax*ratio - this.speedOffset) * Math.min(1, 0.3*dt);
+
+		this.target.copy(this.targetObject.position);
+		this.target.subSelf(this.dir.multiplyScalar(this.zoffset + this.speedOffset));
+		this.target.addSelf(this.up.multiplyScalar(this.yoffset));
+		this.target.y += -this.up.y + this.yoffset;
+		this.camera.position.copy(this.target);
+
+		this.camera.lookAt(this.dir.normalize().multiplyScalar(this.viewOffset).addSelf(this.targetObject.position));
+	} 
+	else if( this.mode == this.modes.ORBIT ) {
+		this.time += dt*.008;
+		this.dir.set(
+			Math.cos(this.time)*this.orbitOffset,
+			this.yoffset/2,
+			Math.sin(this.time)*this.orbitOffset
+		);
+		this.target.copy(this.targetObject.position).addSelf(this.dir);
+		this.camera.position.copy(this.target);
+		this.camera.lookAt(this.targetObject.position);
 	}
 
-	return expose;
+	if( this.cameraCube != null ) {
+		this.cameraCube.rotation.copy(this.camera.rotation);
+	}
 }
